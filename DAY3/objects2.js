773eea8b7081d36430a97c059a7bb314a8799cd8
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-02-05 10:54:59 +0530

    objects & json

--- DAY3/objects2.js
+++ DAY3/objects2.js
@@ -47,11 +47,37 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
+
+const course = {
+    courseName: "js",
+    price: "999",
+    courseInstructor: "abhi"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course
+// console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// {
+//     "name": "abhi",
+//     "coursename": "JS",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
