c169d51cd819c88f3c12fe45a5a9e6004cd4ac2f
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-02-07 10:04:10 +0530

    scope

--- DAY4/scope.js
+++ DAY4/scope.js
@@ -5,11 +5,53 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner: ", a);  // block scope
+    // console.log("Inner: ", a);  // block scope
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c); 
+
+
+function one(){
+    const username = "abhi"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "abhi"
+    if(username === "abhi") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++++ interesting +++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
