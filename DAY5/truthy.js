31a8bd63501e6efe059b18cdab754cf225f4c4b7
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-02-12 22:58:38 +0530

    control flow code files update

--- DAY5/truthy.js
+++ DAY5/truthy.js
@@ -5,3 +5,8 @@ if (userEmail) {
 } else {
     console.log("Don't have user email");
 }
+
+// falsy values => false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values => "0", 'false', " ", [], {}, function(){}
+
