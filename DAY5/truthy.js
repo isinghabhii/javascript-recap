f571fdd05ad66ca53d56e4f8b5c4fef1072c1cbc
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-02-12 23:15:04 +0530

    update

--- DAY5/truthy.js
+++ DAY5/truthy.js
@@ -10,3 +10,32 @@ if (userEmail) {
 
 // truthy values => "0", 'false', " ", [], {}, function(){}
 
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty");
+}
+
+// console.log(false == 0); //true
+// console.log(false == ''); //true
+// console.log(0 == ''); //true
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1);
+
+// Terniary Operator => condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
